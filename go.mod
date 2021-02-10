05dfb5c35b85592951fbd796aaaa4096f9f661a8
Z.Q.K <qiankunzhang@aliyun.com>
2021-02-10 15:11:06 +0800

    multi window

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/zqkgo/gio-practice
+
+go 1.14
+
+require gioui.org v0.0.0-20210208081514-a581acf3fd72
---
+++ window/multi/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"gioui.org/app"
+	"gioui.org/io/system"
+	"gioui.org/unit"
+)
+
+func main() {
+	closed := make(chan struct{}, 100)
+	num := 50
+	for i := 0; i < num; i++ {
+		go newWindow(closed, i)
+	}
+	go func() {
+		time.Sleep(5 * time.Second)
+		for i := 0; i < num; i++ {
+			closed <- struct{}{}
+			time.Sleep(100 * time.Millisecond)
+		}
+		os.Exit(1)
+	}()
+	app.Main()
+}
+
+func newWindow(closed chan struct{}, idx int) {
+	w := app.NewWindow(
+		app.Size(unit.Dp(400), unit.Dp(300)),
+		app.Title(fmt.Sprintf("Window %d", idx)),
+	)
+	for {
+		select {
+		case <-closed:
+			log.Println("send destroy event")
+			w.Close()
+		case e := <-w.Events():
+			if _, ok := e.(system.DestroyEvent); ok {
+				log.Println("destroy event received, quit event loop")
+				return
+			}
+		}
+	}
+}
